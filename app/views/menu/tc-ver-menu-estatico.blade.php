4b4a04a3d80f516720e558ab3bfeae72bbb00e64
eugeMolinero <euge_molinero@yahoo.com.ar>
2015-07-02 15:06:05 -0300

    Estilos Sección estática

--- app/views/menu/tc-ver-menu-estatico.blade.php
+++ app/views/menu/tc-ver-menu-estatico.blade.php
@@ -6,52 +6,17 @@
     @endif
     <section class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 marginBottom2">
                 <h2>{{ $menu -> nombre }}</h2>
             </div>
         </div>
-
-
-        @if(Auth::check())
-            @if(Auth::user()->can("ver_menu_estatico_admin"))
-            <div class="row">
-                <div class="col-md-1"></div>        
-                @if(Auth::user()->can("agregar_slide"))
-                    <div class="col-md-2">
-                        <a href="{{URL::to('admin/slide/agregar/'.$menu->id.'/E')}}" class="btn nuevaSeccion"><i class="fa fa-plus fa-lg"></i>Agregar slide</a>
-                    </div>
-                @endif
-                @if(Auth::user()->can("agregar_slide"))
-                    <div class="col-md-2">
-                        <a href="{{URL::to('admin/slide/agregar/'.$menu->id.'/I')}}" class="btn nuevaSeccion"><i class="fa fa-plus fa-lg"></i>Agregar slide en Index</a>
-                    </div>
-                @endif
-                @if(Auth::user()->can("agregar_galeria"))
-                    <div class="col-md-2">
-                        <a href="{{URL::to('admin/galeria/agregar/'.$menu->id)}}" class="btn nuevaSeccion"><i class="fa fa-plus fa-lg"></i>Agregar galería</a>
-                    </div>
-                @endif
-                @if(Auth::user()->can("agregar_texto"))
-                    <div class="col-md-2">
-                        <a href="{{URL::to('admin/texto/agregar/'.$menu->id)}}" class="btn popup-nueva-seccion"><i class="fa fa-plus fa-lg"></i>Agregar texto</a>
-                    </div>  
-                @endif
-                @if(Auth::user()->can("agregar_html"))
-                    <div class="col-md-2">
-                        <a href="{{URL::to('admin/html/agregar/'.$menu->id)}}" class="btn popup-nueva-seccion"><i class="fa fa-plus fa-lg"></i>Agregar HTML</a>
-                    </div>
-                @endif
-            </div>
-            <div class="clearfix"></div>
-            @endif
-        @endif
-
+        
         @if(Auth::check())
             @if(Auth::user()->can("ordenar_seccion_estatica"))
                 @if(count($menu->secciones) >= 2)
                     <div class="row">
                         <div class="col-md-12">
-                            <a href="{{URL::to('admin/seccion/ordenar-por-menu/'.$menu->id)}}" class="btnSec nuevaSeccion"><i class="fa fa-exchange fa-lg"></i>Ordenar secciones</a>
+                            <a href="{{URL::to('admin/seccion/ordenar-por-menu/'.$menu->id)}}" class="btn pull-right popup-nueva-seccion"><i class="fa fa-exchange fa-lg"></i>Ordenar secciones</a>
                         </div>
                     </div>
                     <div class="clearfix"></div>
@@ -59,6 +24,30 @@
             @endif
         @endif
 
+        @if(Auth::check())
+            @if(Auth::user()->can("ver_menu_estatico_admin"))
+            <div class="row">
+                <div class="col-md-12 fondoDestacado text-center">       
+                    @if(Auth::user()->can("agregar_slide"))
+                       <a href="{{URL::to('admin/slide/agregar/'.$menu->id.'/E')}}" class="btn btn-primary popup-nueva-seccion">Agregar slide</a>
+                    @endif
+                    @if(Auth::user()->can("agregar_slide"))
+                        <a href="{{URL::to('admin/slide/agregar/'.$menu->id.'/I')}}" class="btn btn-primary popup-nueva-seccion">Agregar slide en Index</a>
+                    @endif
+                    @if(Auth::user()->can("agregar_galeria"))
+                        <a href="{{URL::to('admin/galeria/agregar/'.$menu->id)}}" class="btn btn-primary popup-nueva-seccion">Agregar galería</a>
+                    @endif
+                    @if(Auth::user()->can("agregar_texto"))
+                        <a href="{{URL::to('admin/texto/agregar/'.$menu->id)}}" class="btn btn-primary popup-nueva-seccion">Agregar texto</a> 
+                    @endif
+                    @if(Auth::user()->can("agregar_html"))
+                        <a href="{{URL::to('admin/html/agregar/'.$menu->id)}}" class="btn btn-primary popup-nueva-seccion">Agregar HTML</a>
+                    @endif
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            @endif
+        @endif
 
         @foreach($menu -> secciones as $seccion)
             @if((count($seccion->items) > 0) || (count($seccion->slides) > 0))
