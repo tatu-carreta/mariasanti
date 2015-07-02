0bc1446d7125b50a9561729c8f443ba6d0a77e2d
max-ang <max.angletti@gmail.com>
2015-07-02 14:07:20 -0300

    Arreglo de envio de consulta

--- app/views/emails/consulta-contacto.blade.php
+++ app/views/emails/consulta-contacto.blade.php
@@ -7,9 +7,6 @@
 
     <body>
         <div style="max-width: 600px; font-family: sans-serif; font-size:16px; line-height:20px; padding:30px; color: #0E4B70">
-
-            <img src="{{URL::to('images/jma.png')}}" style="width:100%">
-
             <!-- remitente -->
             <p style="font-weight:bold; font-size: 18px">CONSULTA:</p>
             <p>
@@ -22,16 +19,6 @@
                 <strong>Consulta: </strong>{{ $data['consulta'] }}
                 @endif
             </p>
-            <!-- datos empresa -->
-            <div style="border-top:1px solid #ccc;">
-                <p style="font-weight:bold; font-size: 18px">CONTÁCTENOS:</p>
-                <p style="">
-                    <strong>Teléfonos: </strong>Tel: (54 11) 4305-1788 / Fax:--<br>
-                    <strong>Nextel: </strong>-- --<br>
-                    <strong>Email: </strong>info@perfilesjma.com.ar<br>
-                    <strong>Sitio web: </strong><a href="http://perfilesjma.com.ar/">www.perfilesjma.com.ar</a>
-                </p>
-            </div>
         </div>
     </body>
 </html>
